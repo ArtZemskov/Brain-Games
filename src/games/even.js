68745fb3cec91a0f0bc72ab43edfc87f2edf15dd
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-12-01 18:13:50 +0100

    add game even.js

---
+++ src/games/even.js
@@ -0,0 +1,18 @@
+import getRandomInteger from '../randomizer.js';
+import getGameLogic from '../index.js';
+
+const minNumber = 1;
+const maxNumber = 100;
+const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (randomNumber) => randomNumber % 2 === 0;
+
+const getQuestionAndAnswer = () => {
+  const question = getRandomInteger(minNumber, maxNumber);
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const startEvenGame = () => getGameLogic(taskDescription, getQuestionAndAnswer);
+
+export default startEvenGame;
