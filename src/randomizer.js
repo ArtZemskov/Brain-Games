e7a1cb156725e0a576e521e8feb90c5a0a6a19a0
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-12-05 09:25:56 +0100

    update

--- src/randomizer.js
+++ src/randomizer.js
@@ -1,3 +1,17 @@
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+const getRandomSign = (option) => {
+  switch (option) {
+    case 1:
+      return '+';
+    case 2:
+      return '-';
+    case 3:
+      return '*';
+    default:
+      return null;
+  }
+};
+
 export default getRandomInteger;
+export { getRandomSign };
