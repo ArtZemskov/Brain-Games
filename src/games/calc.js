4c2559f44a3b1d36ecdfb42eb710be41c9f994c7
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-12-05 09:31:31 +0100

    add calc game

---
+++ src/games/calc.js
@@ -0,0 +1,33 @@
+import getRandomInteger, { getRandomSign } from '../randomizer.js';
+import getGameLogic from '../index.js';
+
+const taskDescription = 'What is the result of the expression?';
+const minNumber = 1;
+const maxNumber = 25;
+
+const getCalc = (num1, num2, sign) => {
+  switch (sign) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
+  }
+};
+
+const getQuestionAndAnswer = () => {
+  const firstNum = getRandomInteger(minNumber, maxNumber);
+  const secondNum = getRandomInteger(minNumber, maxNumber);
+  const option = getRandomInteger(1, 3);
+  const sign = getRandomSign(option);
+  const question = `${firstNum} ${sign} ${secondNum}`;
+  const answer = `${getCalc(firstNum, secondNum, sign)}`;
+  return [question, answer];
+};
+
+const startCalcGame = () => getGameLogic(taskDescription, getQuestionAndAnswer);
+
+export default startCalcGame;
