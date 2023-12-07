d00c1ad302244d9e45e5b0211cf6de5e1e5560ec
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-12-07 13:32:58 +0100

    add game prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import getRandomInteger from '../randomizer.js';
+import getGameLogic from '../index.js';
+import { isEven } from './even.js';
+
+const taskDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num === 1) return false;
+  if (isEven(num) && num !== 2) return false;
+  if (num % 3 === 0 && num !== 3) return false;
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const randomNum = getRandomInteger(1, 100);
+  const question = `${randomNum}`;
+  const answer = isPrime(randomNum) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const startPrimeGame = () => getGameLogic(taskDescription, getQuestionAndAnswer);
+
+export default startPrimeGame;
