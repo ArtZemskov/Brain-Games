da2ab280e42db7910f23dca02f36211c1d1aca1e
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-12-12 17:03:14 +0100

    delete

--- __tests__/randomizer.test.js
+++
@@ -1,8 +0,0 @@
-import { getRandomSign } from '../src/randomizer.js';
-
-test('should return a math operator', () => {
-  expect(getRandomSign(1)).toBe('+');
-  expect(getRandomSign(2)).toBe('-');
-  expect(getRandomSign(3)).toBe('*');
-  expect(getRandomSign(7)).toBe(null);
-});
