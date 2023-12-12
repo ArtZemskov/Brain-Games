7a2a7fe236c961d97379213a0c9bb7d52fd02237
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-12-12 11:31:02 +0100

    update

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,16 +4,16 @@ import { isEven } from './even.js';
 
 const taskDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (num) => {
+export const isPrime = (num) => {
   if (num === 1) return false;
   if (isEven(num) && num !== 2) return false;
   if (num % 3 === 0 && num !== 3) return false;
   return true;
 };
 
-const getQuestionAndAnswer = () => {
+export const getQuestionAndAnswer = () => {
   const randomNum = getRandomInteger(1, 100);
-  const question = `${randomNum}`;
+  const question = randomNum;
   const answer = isPrime(randomNum) ? 'yes' : 'no';
   return [question, answer];
 };
