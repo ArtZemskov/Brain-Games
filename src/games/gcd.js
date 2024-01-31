e50df8f62796915303491200af6a913f9066ac22
Артем Земсков <artem_zemskov@MacBook-Air.home>
2024-01-31 12:30:46 +0100

    refactor gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,18 +5,7 @@ const taskDescription = 'Find the greatest common divisor of given numbers.';
 const minNumber = 1;
 const maxNumber = 100;
 
-export const getGcd = (num1, num2) => {
-  const min = Math.min(num1, num2);
-  const max = Math.max(num1, num2);
-  let divisor = 0;
-  for (let i = min; i >= 1; i -= 1) {
-    divisor = i;
-    if (min % divisor === 0 && max % divisor === 0) {
-      return divisor;
-    }
-  }
-  return divisor;
-};
+export const getGcd = (num1, num2) => (num1 % num2 === 0 ? num2 : getGcd(num2, num1 % num2));
 
 export const getQuestionAndAnswer = () => {
   const firstNum = getRandomInteger(minNumber, maxNumber);
